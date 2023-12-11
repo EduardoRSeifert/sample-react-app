6d167dead4c07f836f0fa366e0cb5a37bb695a26
Eduardo Rodrigues Seifert <dudaseifert@hotmail.com>
2023-12-11 16:19:05 -0300

    Add check on api key and update readme

--- README.md
+++ README.md
@@ -6,6 +6,13 @@ The idea of this App is to learn how to use api integration in a ReactJs app
 
 It'll fetch gifs from the giphy api and display them in a kendoui grid
 
+### Giphy Api Key
+Please make sure you have a working giphy API key set in your local .env file like this:
+```
+REACT_APP_GIPHY_API_KEY=yourapikeyhere
+```
+If you don't know how to get a giphy key, [Here is a tutorial provided by giphy](https://support.giphy.com/hc/en-us/articles/360020283431-Request-A-GIPHY-API-Key)
+
 ## Running the app
 
 Simply navigate to the root directory and run `npm start`
--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,19 @@
 export default function Home() {
+  const apiKey = process.env.REACT_APP_GIPHY_API_KEY;
+
   return (
     <div>
       <h1>Home Page</h1>
-      <div className="row">
-        <img src="logo512.png" alt="Logo"/>
-      </div>
+      {apiKey ? (
+        <div className="row">
+          <img src="logo512.png" alt="Logo" />
+        </div>
+      ) : (
+        <div className="alert alert-warning">
+          <strong>Warning:</strong> Please set the environment variable
+          REACT_APP_GIPHY_API_KEY to a valid key.
+        </div>
+      )}
     </div>
-  )
+  );
 }
